58443ee6f22b31c04938d246085828a0647edb18
Badavrov <badvrow@gmail.com>
2019-04-10 14:36:08 +0300

    graphql and mongoose added

--- server/index.js
+++ server/index.js
@@ -1,16 +1,44 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer } = require("graphql-yoga");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/portfolio");
+
+const Work = mongoose.model('Work', {
+    title: String,
+    about: String,
+    url: String
+})
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+  type Work {
+      id: ID!
+      title: String!
+      about: String!
+      url: String!
+  }
+  type Mutation{
+      createWork(title: String! about: String! url:String!): Work
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    hello: (_, { name }) => `Hello ${name || "World"}`
   },
-}
+  Mutation: {
+      createWork: async (_, {title, about, url}) =>{
+          const work = new Work({ title, about, url});
+          await work.save();
+          return work;
+      }
+  }
+};
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+mongoose.connection.once("open", function() {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
